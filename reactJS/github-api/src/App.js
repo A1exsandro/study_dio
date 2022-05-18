5db85c3fb010a4fea8be93c00a9c7c49ea790eee
A1exsandro <alexsandro.acp@gmail.com>
2022-05-18 14:09:52 -0400

    create component Layout

--- reactJS/github-api/src/App.js
+++ reactJS/github-api/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Layout>
+        <div>
+          <img source="https://avatars.githubusercontent.com/u/83672739?v=4" alt="Avatar of user" />
+          <h1>Alexsandro Antônio</h1>
+          <h3>UserName: </h3>
+          <span>a1exsandro</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starreds</h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>5</span>
+            </div>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
   );
 }
 
